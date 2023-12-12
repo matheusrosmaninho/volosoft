0c4dc453d78453053096f63568cc6b204c6f86bb
Matheus Rosmaninho <matheusrosmano@gmail.com>
2023-12-12 19:58:32 -0300

    fix: Restringineo paginas

--- src/Acme.BookStore.Web/Menus/BookStoreMenuContributor.cs
+++ src/Acme.BookStore.Web/Menus/BookStoreMenuContributor.cs
@@ -1,6 +1,8 @@
 ﻿using System.Threading.Tasks;
 using Acme.BookStore.Localization;
 using Acme.BookStore.MultiTenancy;
+using Acme.BookStore.Permissions;
+using Volo.Abp.Authorization.Permissions;
 using Volo.Abp.Identity.Web.Navigation;
 using Volo.Abp.SettingManagement.Web.Navigation;
 using Volo.Abp.TenantManagement.Web.Navigation;
@@ -48,7 +50,8 @@ public class BookStoreMenuContributor : IMenuContributor
 
         context.Menu
             .AddItem(new ApplicationMenuItem( "BooksStore", l["Menu:BookStore"], icon: "fa fa-book" )
-            .AddItem(new ApplicationMenuItem( "BooksStore.Books", l["Menu:Books"], url: "/Books" )));
+            .AddItem(new ApplicationMenuItem( "BooksStore.Books", l["Menu:Books"], url: "/Books" )
+            .RequirePermissions(BookStorePermissions.Books.Default)));
 
 
         return Task.CompletedTask;
